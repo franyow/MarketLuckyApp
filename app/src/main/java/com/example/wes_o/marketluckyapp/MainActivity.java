c9d6eb4572806d36acdbf18bc1e17b147a50cbcd
Franyo <franyow@gmail.com>
2018-04-04 17:56:36 -0500

    Initial commit

---
+++ app/src/main/java/com/example/wes_o/marketluckyapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.wes_o.marketluckyapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
